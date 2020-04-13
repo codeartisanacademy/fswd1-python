aa4a9b74dd4d3c33980ff6edb8231edadd3253d5
wahyudi <wahyudi.coding@gmail.com>
2020-04-13 20:50:10 +0700

    function, dictionary

---
+++ functionbasic.py
@@ -0,0 +1,27 @@
+def say_hi():
+    print('hi')
+
+say_hi()
+
+def salam(name):
+    print("Hallo {0}, salam".format(name))
+
+salam('Rudi')
+
+def add(x, y):
+    return x + y
+
+result = add(2, 3)
+print(result)
+
+print("3 + 3 = {0}".format(add(3,3)))
+
+print("4 + 3 = {0}".format(add(4,3)))
+
+print("4 + 9 = {0}".format(add(4,9)))
+
+# function untuk mengkalkulasi besarnya bayaran bulanan
+# tanya user harga yang mau dibeli
+# tanya user berapa lama mau melunasi
+
+# 
